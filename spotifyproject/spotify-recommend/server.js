727c6e54a47e83f4bb2970f9b3b683898a8b8de2
Adam Tenhouse <merthedil@gmail.com>
2016-09-08 15:00:45 -0500

    Used example code to pass the hurdle - CheckComplete did the trick

--- spotifyproject/spotify-recommend/server.js
+++ spotifyproject/spotify-recommend/server.js
@@ -2,38 +2,32 @@ var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
 
+var app = express();
+app.use(express.static('public'));
+
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('return', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+        .qs(args)
+        .end(function(response) {
+            emitter.emit('end', response.body);
+        });
     return emitter;
 };
 
-var getRelatedArtists = function(artistID, args) {
-    var emitter = new events.EventEmitter();
-    unirest.get('https://api.spotify.com/v1/artists/' + artistID + '/related-artists')
-           .qs(args)
-           .end(function(response) {
-                if (response) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
-    return emitter;
+var getTracks = function(artist, cb) {
+    // Please note: Country is a REQUIRED parameter according to Spotify documentation
+    unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/top-tracks')
+        .qs({country: "us"})
+        .end(function(response) {
+            if (!response.error) {
+                artist.tracks = response.body.tracks;
+                cb();
+            } else {
+                cb(response.error);
+            }
+        });
 };
-
-var app = express();
-app.use(express.static('public'));
 
 app.get('/search/:name', function(req, res) {
     var searchReq = getFromApi('search', {
@@ -41,27 +35,47 @@ app.get('/search/:name', function(req, res) {
         limit: 1,
         type: 'artist'
     });
-    searchReq.on('return', function(item) {
+
+    searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        var related_artist = getRelatedArtists(artist.id);
+        unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/related-artists')
+            .end(function(response) {
+                if (!response.error) {
+                    artist.related = response.body.artists;
 
-        related_artist.on('end', function(item){
-            // console.log("related artist ended.", item.artists)
-            artist.related = item.artists;
-            // console.log(artist);
-            res.json(artist);
-        });
+                    var totalArtists = artist.related.length;
+                    var completed = 0;
 
-        related_artist.on('error', function(item){
-            console.log("Error thrown:" + item);
-            // console.log(artist);
-            res.json(artist);
-        });
+                    var checkComplete = function() {
+                        if (completed === totalArtists) {
+                            res.json(artist);
+                        }
+                    };
+
+                    artist.related.forEach(function(artist) {
+                        getTracks(artist, function(err) {
+                            if (err) {
+                                res.sendStatus(404);
+                            }
+
+                            completed += 1;
+                            checkComplete();
+
+                        });
+                    });
+
+                } else {
+                    res.sendStatus(404);
+                }
+
+            });
+
     });
 
-    searchReq.on('error', function(code) {
-        res.sendStatus(code);
+    searchReq.on('error', function() {
+        res.sendStatus(404);
     });
+
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(3000);
