3bd6b6dd61576ce80789863b26ff9e5911db6c2f
Adam Tenhouse <merthedil@gmail.com>
2016-09-22 19:11:59 +0000

    Beginning to add logic for guess v draw ...

--- Unit5/pictionary/public/main.js
+++ Unit5/pictionary/public/main.js
@@ -1,10 +1,28 @@
 var pictionary = function() {
+    
+    var WORDS = [
+        "word", "letter", "number", "person", "pen", "class", "people",
+        "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+        "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+        "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+        "brother", "sister", "world", "head", "page", "country", "question",
+        "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+        "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+        "west", "child", "children", "example", "paper", "music", "river", "car",
+        "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+        "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+        "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+        "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+        "space"
+    ];
+    
     var socket = io();
     var canvas, context;
     // defaulting to false ...
     var drawing = false;
     
     var guessBox = $('#guessInput');
+    var guessSpan = $("#guess span");
 
     var onKeyDown = function(event) {
         if (event.keyCode != 13) { // Enter
@@ -14,7 +32,7 @@ var pictionary = function() {
             socket.emit('guess', guessBox.val());
         }
         guessBox.val('');
-    }
+    };
     
     guessBox.on('keydown', onKeyDown);
     
@@ -35,8 +53,9 @@ var pictionary = function() {
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
+    
     canvas.on('mousemove', function(event) {
-        if(!drawing){
+        if(!drawing) {
             return
         }
         var offset = canvas.offset();
@@ -54,8 +73,29 @@ var pictionary = function() {
         drawing = false;
     });
     
+    var rolesdiv = $('#roles');
+
+    var assignDraw = function() {
+      rolesdiv.text("Drawer");
+      socket.role = "drawer";
+      alert("DRAW SOMETHING");
+      guessSpan.html('Please select a word from the list: ');
+      context.clearRect(0,0,600,400);
+    };
+
+    var assignGuess = function() {
+      socket.role = "guesser";
+      rolesdiv.text("Guesser");
+      alert("MAKE A GUESS!");
+      guessSpan.html('Make a guess:');
+      context.clearRect(0,0,600,400);
+    };
+    
     socket.on('draw', draw);
     socket.on('guess', makeGuess);
+    socket.on('drawer', assignDraw);
+    socket.on('guesser', assignGuess);
+    
 };
 
 $(document).ready(function() {
